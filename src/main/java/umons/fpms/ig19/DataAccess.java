874b25077786354c3a79ba85b5047535e5bea3fd
NOFFABEL <fabricenolack@yahoo.fr>
2020-10-19 15:29:20 +0200

    Update DataAccess.java

--- src/main/java/umons/fpms/ig19/DataAccess.java
+++ src/main/java/umons/fpms/ig19/DataAccess.java
@@ -25,7 +25,7 @@ public class DataAccess {
     public static final String DBNAME = "test_java";
     private static final String DBUSER = "root";
     private static final String DBPASS = "sql-db-2020";
-    private static final String DBHOST = "localhost";
+    private static final String DBHOST = "192.168.250.3";
     private static final String DBSERVER = "mysql";
     private static final String DBPORT = "3306";
     private static final String DSN = "jdbc:" + DBSERVER + "://" + DBHOST + ":" + DBPORT + "/" + "?useUnicode=true" +
